34bf064ba5afff26dc9a0607a48893c654a834c6
Lucas Leme <lucasratm40@gmail.com>
2022-08-19 18:49:40 -0300

    Create the Recent posts and related posts card

--- services/index.js
+++ services/index.js
@@ -36,4 +36,49 @@ export const getPosts = async () => {
   const result = await request(graphqlAPI, query);
 
   return result.postsConnection.edges;
-}
+};
+
+export const getRecentPosts = async () => { //to get the newest posts
+  const query = gql `
+    query GetPostDetails() { 
+      posts(
+        orderBy: createdAt_ASC
+        last: 3  
+      ){
+        title
+        featuredImage{
+          url 
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+  
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
+
+export const getSimilarPost = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]){
+      posts(
+        where: { slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+      ){
+        title
+        featuredImage{
+          url 
+        }
+        createdAt
+        slug
+      }
+
+    }
+  `
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
