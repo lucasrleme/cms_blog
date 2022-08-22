7d58bda12b8327913e117570eef348eaaa582b18
Lucas Leme <lucasratm40@gmail.com>
2022-08-22 15:04:35 -0300

    Create and show the commentForm component / send the comments to GraphCMS

---
+++ pages/api/comments.js
@@ -0,0 +1,32 @@
+// Any file inside the folder pages/api is mapped to /api/* and will be treated as an API endpoint instead of a page
+
+import {GraphQLClient, gql} from 'graphql-request'
+import { request } from 'http';
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const graphcmsToken = process.env.GRAPHCMS_TOKEN;
+
+export default async function comments(req, res) {
+  const graphQLClient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      authorization: `Bearer ${graphcmsToken}`
+    },
+  });
+
+  //add or alter some data
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+      createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) { id }
+    }
+  `;
+
+const result = await graphQLClient.request(query, {
+  name: req.body.name,
+  email: req.body.email,
+  comment: req.body.comment,
+  slug: req.body.slug,
+});
+
+
+  return res.status(200).send(result);
+}
