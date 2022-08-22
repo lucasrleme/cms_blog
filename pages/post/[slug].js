cb692b1af55e6cc26c60aee298d022fe5fa51c2f
Lucas Leme <lucasratm40@gmail.com>
2022-08-22 18:00:11 -0300

    Reorder the queries of services and making the last fixings to publish the project

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,10 +1,18 @@
 import React from 'react'
 
+import {useRouter} from 'next/router'
+
 import { getPosts , getPostDetails } from '../../services'
 
-import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components'
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components'
 
 const PostDetails = ({post}) => {
+  const router = useRouter();
+
+  if(router.isFallback){
+    return <Loader />
+  }
+  
   return (
     <div className='container mx-auto px-10 mb-8'>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
@@ -44,6 +52,6 @@ export async function getStaticPaths(){
   const posts = await getPosts();
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: false,
+    fallback: true,
   }
 }
