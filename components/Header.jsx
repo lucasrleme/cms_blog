57d1572485df43b6ab6a641abba9636e3723c97a
Lucas Leme <lucasratm40@gmail.com>
2022-08-23 15:49:26 -0300

    background change and some bug fixes

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,11 +14,11 @@ const Header = () => {
 
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
+      <div className="border-b w-full inline-block border-gray-300 py-8">
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Graph CMS
+              CMS Blog
             </span>
           </Link>
         </div>
@@ -26,7 +26,7 @@ const Header = () => {
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="md:float-right mt-2 align-midle text-white ml-4 font-semibold cursor-pointer">
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
             </Link>
