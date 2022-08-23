16ef37d923051cb56209217907e1a7446eae6af6
Lucas Leme <lucasratm40@gmail.com>
2022-08-23 17:37:04 -0300

    change the object-position of card image

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -9,7 +9,7 @@ const PostCard = ({ post }) => {
         <img 
           src={post.featuredImage.url} 
           alt={post.title}
-          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg" 
+          className="object-center absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg" 
         />
       </div>
 
